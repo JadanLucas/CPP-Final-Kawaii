59fdfd9f301fd2bfaafb9d70c5b04e530e61db31
JadanLucas <31167432+JadanLucas@users.noreply.github.com>
2018-05-03 14:20:25 -0600

    Basic Start

--- clint.cpp
+++ clint.cpp
@@ -4,16 +4,28 @@
 using namespace std;
 int main(int argc, char *argv[]) {
 	
-	string name = "";
+	/*
+	Name | Blue
+	Narrator | Green
+	Amazon Alexa | 95 Magenta
+	Steve Clinton | Brown/Yellow
+	*/
 	
+	
+	string name = "\033[1;96m-";
+	string color = "";
+	string narrator = "\033[1;92m-Narrator: ";
+	//Narrator Speach
 	cout << "Please Enter your desired name: ";
 	cin >> name; 
-	
-	cout << "" << endl;
 	cout << endl;
+	
+	cout << narrator << "\033[1;92mHello, I am your Narrator! I will assist you towards your special moment with a beauty of a woman.\033[0m\n" << endl;
+	
+	//Characters first decision
 	cout << name << ": ..." << endl;
-	cout << "1 | (talk to her)" << endl;
-	cout << "2 | (finish your coffe and leave)" << endl;
+	cout << "0 | (talk to her)" << endl;
+	cout << "0 | (finish your coffe and leave)" << endl;
 	
 	 
 }
