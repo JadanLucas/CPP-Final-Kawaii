74b546e393e84528acc95a406f7f81681cc5f8be
OwOma <31140912+HopeMan0@users.noreply.github.com>
2018-05-03 13:32:15 -0600

    Add files via upload
    This is the very first version

---
+++ clint.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	
+	string name = "";
+	
+	cout << "Enter your name: ";
+	cin >> name; 
+	
+	cout << "(While relaxing in a coffee shop, a beautiful girboy catches your eye)" << endl;
+	cout << endl;
+	cout << name << ": ..." << endl;
+	cout << "1 | (talk to her)" << endl;
+	cout << "2 | (finish your coffe and leave)" << endl;
+	
+	 
+}
